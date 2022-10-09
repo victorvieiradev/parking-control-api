d53a8dbe961d91e3425aa1cbcb22ee58c76751e4
victorvieiradev <contatovictorvieira@outlook.com>
2022-10-09 13:55:01 -0300

    Finalizado a classe ParkingSpotService

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrol.controllers;
+
+import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+    private final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+}
